eed91fe1d59998fb8d1d87cc8533e97e6c7867f8
Adham Mohamed Sabry Zineldin <mohalya3@gmail.com>
2025-04-11 15:52:24 +0200

    Add initial backend structure and configuration files for FalsoPay project

--- backend/routes/api.php
+++ backend/routes/api.php
@@ -5,3 +5,10 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/health_check', function (){
+    return response()->json([
+        'status' => 'ok',
+        'message' => 'Server is running'
+    ]);
+});

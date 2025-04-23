e31a3ea6665ec5ba6f3b79f8e3274189f894db81
Adham Mohamed Sabry Zineldin <mohalya3@gmail.com>
2025-04-23 19:32:59 +0200

    Add CORS headers to Router; handle preflight requests in handleRequest method

--- backend/core/Router.php
+++ backend/core/Router.php
@@ -21,6 +21,7 @@ class Router
         header("Access-Control-Allow-Origin: *");
         header("Access-Control-Allow-Headers: Content-Type, Authorization");
         header("Access-Control-Allow-Methods: GET, POST, PUT, DELETE, OPTIONS");
+        header("Access-Control-Allow-Credentials: true");
     }
 
 
--- backend/server.php
+++ backend/server.php
@@ -21,7 +21,7 @@ use JetBrains\PhpStorm\NoReturn;
 header("Access-Control-Allow-Origin: *");
 header("Access-Control-Allow-Headers: Content-Type, Authorization");
 header("Access-Control-Allow-Methods: GET, POST, PUT, DELETE, OPTIONS");
-
+header("Access-Control-Allow-Credentials: true");
 // Handle pre-flight requests (OPTIONS)
 if ($_SERVER['REQUEST_METHOD'] === 'OPTIONS') {
     http_response_code(200);

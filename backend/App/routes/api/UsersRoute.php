83944818da08c389bcf2fab23396a3a12acd715d
Adham Mohamed Sabry Zineldin <mohalya3@gmail.com>
2025-04-24 14:53:51 +0200

    Remove unnecessary middlewares from user-related routes in UsersRoute.php

--- backend/App/routes/api/UsersRoute.php
+++ backend/App/routes/api/UsersRoute.php
@@ -17,14 +17,14 @@ class UsersRoute
         $router->add('DELETE', '/api/users/{id}', [UserController::class, 'deleteUser'], $middlewares);
 
         // Additional routes for user email 
-        $router->add('GET', '/api/users/email/{email}', [UserController::class, 'getUserByEmail'], $middlewares);
-        $router->add('GET', '/api/users/number/{number}', [UserController::class, 'getUserByPhoneNumber'], $middlewares);
+        $router->add('GET', '/api/users/email/{email}', [UserController::class, 'getUserByEmail']);
+        $router->add('GET', '/api/users/number/{number}', [UserController::class, 'getUserByPhoneNumber']);
         
         // Route for checking if a user exists by phone number
-        $router->add('GET', '/api/users/exists/{phone_number}', [UserController::class, 'checkUserExistsByPhoneNumber'], $middlewares);
+        $router->add('GET', '/api/users/exists/{phone_number}', [UserController::class, 'checkUserExistsByPhoneNumber']);
         
         // Route for getting and setting the default account for a user
-        $router->add('GET', '/api/users/{id}/default-account', [UserController::class, 'getDefaultAccount'], $middlewares);
+        $router->add('GET', '/api/users/{id}/default-account', [UserController::class, 'getDefaultAccount']);
         $router->add('PUT', '/api/users/{id}/default-account', [UserController::class, 'setDefaultAccount'], $middlewares);
     }
 }
